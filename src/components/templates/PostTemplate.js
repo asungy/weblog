715000ddf30070bbf2b463aa3961c562c0a2d62f
Alek Sung <aleksungis@gmail.com>
2021-06-28 18:55:30 -0400

    Added syntax highlighting support for code blocks in MDX

--- src/components/templates/PostTemplate.js
+++ src/components/templates/PostTemplate.js
@@ -2,8 +2,37 @@ import React from 'react';
 import { graphql } from 'gatsby';
 import { MDXRenderer } from 'gatsby-plugin-mdx';
 import { MDXProvider } from '@mdx-js/react';
+import Highlight, { defaultProps } from 'prism-react-renderer';
+import theme from 'prism-react-renderer/themes/dracula';
 
 import Page from '../Page';
+
+// Override <pre> tag to support code syntax highlighting
+const CodeBlock = (props) => {
+  // Get class name of children elements
+  const className = props.children.props.className || '';
+  // Capture language used in children elements
+  const matches = className.match(/language-(?<lang>.*)/);
+  // I have no idea what's going on here
+  // Reference: https://github.com/FormidableLabs/prism-react-renderer/blob/master/README.md
+  return (
+    <Highlight {...defaultProps} code={props.children.props.children.trim()} language={
+        matches && matches.groups && matches.groups.lang ? matches.groups.lang : ''
+      } theme={theme}>
+      {({className, style, tokens, getLineProps, getTokenProps}) => (
+        <pre className={className} style={{...style, padding: '20px'}}>
+          {tokens.map((line, i) => (
+            <div key={i} {...getLineProps({line, key: i})}>
+              {line.map((token, key) => (
+                <span key={key} {...getTokenProps({token, key})} />
+              ))}
+            </div>
+          ))}
+        </pre>
+      )}
+    </Highlight>
+  );
+};
 
 export const query = graphql`
   query($id: String) {
@@ -21,6 +50,7 @@ export const query = graphql`
 // Override default tags with custom components
 // For more information: https://www.gatsbyjs.com/plugins/gatsby-plugin-mdx/#components
 const customTags = {
+  pre: CodeBlock,
 }
 
 const PostTemplate = ({ data }) => {
